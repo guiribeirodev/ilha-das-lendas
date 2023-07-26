115549dfb0b277d11401e840913af79c0a901f85
guiribeirodev <guiribeirodev@gmail.com>
2023-07-25 23:07:01 -0300

    Initial project structure

---
+++ app/header.tsx
@@ -0,0 +1,19 @@
+export default function header(){
+    return (
+        <header>
+      <p>Logo aqui</p>
+
+      <nav>
+        <ul>
+          <li>
+            Cartinhas
+          </li>
+          <li>
+            MultiStream
+          </li>
+        </ul>
+      </nav>
+      
+    </header>
+    )
+}
